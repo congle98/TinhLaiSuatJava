e4a0091fc3c56ce5f131841f279a9fcc36f79a0a
congle98 <lethanhcongcnhn@gmail.com>
2021-04-02 16:49:10 +0700

    add file

---
+++ src/demo.java
@@ -0,0 +1,29 @@
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        double money;
+        int month;
+        double interset_rate;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.println("Enter number of months: ");
+        month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interset_rate = input.nextDouble();
+        double total_interset = 0;
+        for(int i = 0; i < month; i++){
+            total_interset += money * (interset_rate/100)/12 * month;
+
+        }
+        Locale locale = new Locale("vi","VN");
+        NumberFormat format = NumberFormat.getCurrencyInstance(locale);
+        format.setRoundingMode(RoundingMode.HALF_UP);
+
+        System.out.println("Total of interset: " + format.format(total_interset));
+    }
+}
